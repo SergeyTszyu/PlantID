b9a21c69747600190fcd4fcc8cd6078b7f9b73dd
SergeyTsz <Sergey.Tszyu@gmail.com>
2024-11-26 22:23:24 +0000

    addded scanner analyzer

---
+++ Measure/Sources/UI/Scanner/ScannerAnalyzer/ScannerAnalyzerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ScannerAnalyzerViewController.swift
+//  PlantNEW
+
+import UIKit
+import Lottie
+
+class ScannerAnalyzerViewController: BaseViewController {
+    
+    @IBOutlet private weak var scanningImageView: UIImageView!
+//    @IBOutlet private var animationScanView: UIView!
+    
+    var scanningImage: UIImage!
+    private var animationView: LottieAnimationView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scanningImageView.image = scanningImage
+        
+        animationView = .init(name: "AnimationScan")
+        animationView!.frame = scanningImageView.bounds
+        animationView!.contentMode = .scaleAspectFill
+        animationView!.loopMode = .loop
+        animationView!.animationSpeed = 1.0
+        scanningImageView.addSubview(animationView!)
+        animationView!.play()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+}
